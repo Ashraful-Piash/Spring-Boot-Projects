9ef0738429935c9f712c2e38becdbe1d9085460c
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-02-05 22:43:22 +0600

    Initial commit

---
+++ Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/SpringBootLearingApplication.java
@@ -0,0 +1,13 @@
+package com.piashraful.Spring.boot.learing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearingApplication.class, args);
+	}
+
+}
---
+++ Spring-boot-learing/Spring-boot-learing/src/test/java/com/piashraful/Spring/boot/learing/SpringBootLearingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piashraful.Spring.boot.learing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
