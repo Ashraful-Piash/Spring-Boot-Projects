50a4ab58eb160442332230da96eff939f67d3374
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-02-06 23:02:02 +0600

    Changed

--- Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/SpringBootLearningApplication.java
+++ Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/SpringBootLearningApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLearingApplication {
+public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLearingApplication.class, args);
+		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
 
 }
--- Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/controller/HelloController.java
+++ Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/controller/HelloController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld(){
-        return "Hello World";
-    }
+
+
 }
+
