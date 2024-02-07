32122422113be199cb4fb0e78d424a9dead26e48
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-02-07 20:48:43 +0600

    changed

--- Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/controller/HelloController.java
+++ Spring-boot-learing/Spring-boot-learing/src/main/java/com/piashraful/Spring/boot/learing/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String helloMessage() {
-        return "Hello World. I will be the best in sha Allah. 1134";
+        return "Hello Message";
     }
 
 }
