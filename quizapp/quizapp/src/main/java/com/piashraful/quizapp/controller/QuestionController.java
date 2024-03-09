e29beab43a55b4498aa3674e113052eb6e6eee43
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-03-09 20:53:07 +0600

    Updated

--- quizapp/quizapp/src/main/java/com/piashraful/quizapp/controller/QuestionController.java
+++ quizapp/quizapp/src/main/java/com/piashraful/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.piashraful.quizapp.controller;
 import com.piashraful.quizapp.entity.Question;
 import com.piashraful.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +15,32 @@ public class QuestionController {
     @Autowired
     private QuestionService questionService;
 
+    @PostMapping("/add")
+    public ResponseEntity<List<String>> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
     @GetMapping("/allQuestions")
-    public List<Question> allQuestions() {
+    public ResponseEntity<List<Question>> allQuestions() {
         return questionService.getAllQuestions();
     }
 
 
-   @GetMapping("/allQuestions/category/{category}")
-    public List<Question> findQuestionByCategory(@PathVariable("category") String category){
+    @GetMapping("/allQuestions/category/{category}")
+    public List<Question> findQuestionByCategory(@PathVariable("category") String category) {
         return questionService.findQuestionByCategory(category);
-   }
+    }
 
-   @GetMapping("/allQuestions/difficulty/{difficulty}")
-    public List<Question> findQuestionByDifficulty(@PathVariable("difficulty") String difficultyLevel){
+    @GetMapping("/allQuestions/difficulty/{difficulty}")
+    public List<Question> findQuestionByDifficulty(@PathVariable("difficulty") String difficultyLevel) {
         return questionService.findQuestionByDifficulty(difficultyLevel);
-   }
+    }
 
+    @DeleteMapping("/delete/{id}")
+    public void deleteQuestionById(@PathVariable("id") int id) {
+        questionService.deleteQuestionById(id);
+
+    }
 
 
 }
--- quizapp/quizapp/src/main/java/com/piashraful/quizapp/service/QuestionService.java
+++ quizapp/quizapp/src/main/java/com/piashraful/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.piashraful.quizapp.service;
 import com.piashraful.quizapp.dao.QuestionDao;
 import com.piashraful.quizapp.entity.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,8 +15,13 @@ public class QuestionService {
     @Autowired
     private QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 
     }
 
@@ -24,5 +32,19 @@ public class QuestionService {
 
     public List<Question> findQuestionByDifficulty(String difficultyLevel) {
         return questionDao.findByDifficultyLevelIgnoreCase(difficultyLevel);
+    }
+
+    public ResponseEntity<List<String>>addQuestion(Question question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    public void deleteQuestionById(int id) {
+        questionDao.deleteById(id);
     }
 }
