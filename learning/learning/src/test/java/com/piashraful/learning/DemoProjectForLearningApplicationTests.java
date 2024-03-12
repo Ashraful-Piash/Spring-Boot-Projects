ce34eea4776cd4c38738b6f827207a35007cd66f
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-03-12 15:33:23 +0600

    Initialezed new project

---
+++ learning/learning/src/test/java/com/piashraful/learning/DemoProjectForLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piashraful.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
