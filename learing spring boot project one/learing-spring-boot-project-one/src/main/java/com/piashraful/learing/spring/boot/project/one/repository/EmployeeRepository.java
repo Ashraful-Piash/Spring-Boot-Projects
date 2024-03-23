259d6a71ad5e03b2c09a91a4a568ce5b306a29f4
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-03-23 23:32:35 +0600

    updated

--- learing spring boot project one/learing-spring-boot-project-one/src/main/java/com/piashraful/learing/spring/boot/project/one/repository/EmployeeRepository.java
+++ learing spring boot project one/learing-spring-boot-project-one/src/main/java/com/piashraful/learing/spring/boot/project/one/repository/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.piashraful.learing.spring.boot.project.one.repository;
 
 import com.piashraful.learing.spring.boot.project.one.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
